26ba4ff6fb2e1f06ee0ea6e8fc2fbc6e25db3a5c
Mauro Rodrigues <mauro.developer.br@gmail.com>
2023-10-02 09:16:34 +0200

    firt commit

---
+++ src/main/java/com/example/eventfinder/EventFinderApiApplication.java
@@ -0,0 +1,13 @@
+package com.example.eventfinder;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EventFinderApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EventFinderApiApplication.class, args);
+	}
+
+}
